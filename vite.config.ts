52dbacc87b705c5c52110edce172a229fa100013
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-18 09:02:08 -0500

    Begin from Pwa-Deploy repo

---
+++ vite.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {VitePWA} from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  base: "/Pwa-Test-Harness/",
+  publicDir: "public",
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto', // https://vite-pwa-org.netlify.app/guide/register-service-worker.html
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Lab.Pwa-Debug',
+        short_name: 'PwaDebug',
+        description: 'Testing my Pwa Debug on Device setup ✌️',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
+})
